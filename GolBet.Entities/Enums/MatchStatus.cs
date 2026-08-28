6300ffb47c703e3a333d1ff2fc0152dbdc41d3ce
Andres <andresfelipe4277@gmail.com>
2026-08-27 20:04:26 -0500

    Add domain entities, AppDbContext and initial migration (Module 2)

---
+++ GolBet.Entities/Enums/MatchStatus.cs
@@ -0,0 +1,16 @@
+﻿// GolBet.Entities/Enums/MatchStatus.cs 
+
+namespace GolBet.Entities.Enums;
+
+public enum MatchStatus
+
+{
+
+    Scheduled = 0,
+
+    InProgress = 1,
+
+    Finished = 2
+
+}
+
